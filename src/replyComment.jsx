a2cdfab5b9c588442ea25d4a1accc85846775f53
Dicksen Tan <tandicksen@gmail.com>
2024-03-09 21:11:22 +0700

    add: add self comment component

---
+++ src/replyComment.jsx
@@ -0,0 +1,12 @@
+function replyComment(){
+    return(
+        <form action="" className="myComment">
+            <input type="text" className="content"/>
+            <div className="myComment-details">
+                <img src="" alt="" className="profile" />
+                <button>Send</button>
+            </div>
+        </form>
+    )
+}
+export default replyComment
