497f87faf7d6d85df689e374dd1591e75f0387af
Dicksen Tan <tandicksen@gmail.com>
2024-03-23 20:37:04 +0700

    Add: delete comment function

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -3,11 +3,13 @@ import minus from '/icon-minus.svg'
 import reply from '/icon-reply.svg'
 import { useEffect, useState } from 'react'
 import ReplyComment from './ReplyComment'
-import useFetch from './useFetch'
+import deleteIcon from '/icon-delete.svg'
+import editIcon from '/icon-edit.svg'
 
 
 function Comment(props){
     const data = props.data
+    const [username, setUsername] = useState(props.username)
     const [count, setCount] = useState(data.score);
     const [replyData, setReplyData] = useState(data.replies)
     const [liked, setLiked] = useState(false)
@@ -45,7 +47,6 @@ function Comment(props){
     const handleReply = (name) =>{
         setReplying(true)
         setReplyName(name)
-        //props.setReplyActive(true)
     }
   
 
@@ -62,13 +63,17 @@ function Comment(props){
     const text = data.content
     const pattern =/((^|\s)@[\w-]+)/g;
     const parts = text.split(pattern)
+
+    const deleteReplies= (id) =>{
+        setReplyData(prevData => prevData.filter(comment => comment.id !== id))
+    } 
     return(
     <>
     <article className={`comment ${props.type}`}>
         <div className="details">
             <img src={data.user.image.webp} alt="" className="profile" />
             <h3 className="name">{data.user.username}</h3>
-            
+            {props.username === data.user.username ?(<div className='you'>you</div>) : ''}
             <p className="time">{data.createdAt}</p>
         </div>
         <p className="content">
@@ -88,15 +93,27 @@ function Comment(props){
                 
                 <img src={minus} alt="" className="minus" onClick={likeHandle} />
             </div>
-            <div className="reply" onClick={props.type === 'replies' ? (() => props.reply(data.user.username)) : (() => handleReply(data.user.username))}>
+            {props.username === data.user.username ?(
+            <div className='action-container'>
+                <button className="deleteComment" onClick={() => props.delete(data.id)}>
+                    <img src={deleteIcon} alt="" />
+                    Delete
+                </button>
+                <button className='editComment'>
+                    <img src={editIcon} alt="" />Edit
+                </button>
+            </div>) 
+            : 
+            (<div className="reply" onClick={props.type === 'replies' ? (() => props.reply(data.user.username)) : (() => handleReply(data.user.username))}>
                 <img src={reply} alt="" className="replyImg" />
                 <p className="reply-para">Reply</p>
-            </div>
+            </div>)}
+            
         </div>
     </article>
     <div className=" comments  replies-container">
     {replyData && replyData.map((rpl) =>(
-        <Comment data={rpl} key={rpl.id} type={'replies'} reply={handleReply}/>
+        <Comment data={rpl} key={rpl.id} type={'replies'} reply={handleReply} username={username} delete={deleteReplies}/>
     ))}
     {replying && <ReplyComment name={replyName} data={props.user} addReply={addReplies} type={'replies'} handleCancel={handleCancel}/>}
     </div>
--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,14 +1,34 @@
 import { useEffect, useState } from 'react'
 import useFetch from './useFetch'
 import Comment from './Comment'
+import ReplyComment from './ReplyComment'
 
 function Comments(){
-    const fetchedData = useFetch('data.json')
+    const {fetchedComment, fetchedUser} = useFetch('data.json')
+    const [data, setData] = useState()
+    const [user, setUser] = useState()
+    useEffect(() =>{
+        setData(fetchedComment)
+    },[fetchedComment])
+    useEffect(() =>{
+        if(fetchedUser){
+        setUser(fetchedUser.username)
+        }
+    },[fetchedUser])
+    const addComment= (commentData)=>{
+        setData(prevData => [...prevData, commentData])
+    }
+
+    const deleteComment = (id) =>{
+        setData(prevData => prevData.filter((comment) => comment.id !== id))
+        console.log(id)
+    }
     return(
         <section className="comments">
-            {fetchedData && fetchedData.comments.map((cdt)=>(
-                <Comment data={cdt} key={cdt.id}/>
+            {data && data.map((cdt)=>(
+                <Comment data={cdt} key={cdt.id} user={fetchedUser} username={user} delete={deleteComment}/>
             ))}
+            {fetchedUser && <ReplyComment data={fetchedUser} addReply={addComment} name={''}/>}
         </section>
     )
 }
