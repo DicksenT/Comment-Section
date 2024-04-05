7158720e32cc65acec130cdcee322136a0cc5221
Dicksen Tan <tandicksen@gmail.com>
2024-04-05 22:28:30 +0700

    Update Comments.jsx

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -4,15 +4,14 @@ import Comment from './Comment'
 import ReplyComment from './ReplyComment'
 
 function Comments(props){
-    const {data, user, addComment} = props
-
+    const {data, user, addComment, deleteComment} = props
 
     return(
         <section className="comments">
             {data.map((cdt)=>(
-                <Comment data={cdt} key={cdt.id} user={user} username={user.username} delete={props.deleteComment}/>
+                <Comment data={cdt} key={cdt.id} user={user} username={user.username} delete={deleteComment}/>
             ))}
-            {<ReplyComment data={user} addReply={props.addComment} name={''}/>}
+            {<ReplyComment data={user} addReply={addComment} name={''}/>}
         </section>
     )
 }
